8f41bb41eadd91af24ff3ffe85deb03b0c46556e
Cade Ferguson <cade_ferguson1@baylor.edu>
2021-10-04 17:30:11 -0500

    done

--- json1.py
+++ json1.py
@@ -1,6 +1,6 @@
 import json
 
-infile = open('eq_data_1_day_m1.json','r')
+infile = open('eq_data_30_day_m1.json','r')
 outfile = open("readable_eq_data.json",'w')
 
 eqdata = json.load(infile)
@@ -15,12 +15,23 @@ list_of_eqs = eqdata["features"]
 
 for eq in list_of_eqs:
     mag = eq["properties"]["mag"]
-    lat = eq["geometry"]["coordinates"][0]
-    lon = eq["geometry"]["coordinates"][1]
+    lat = eq["geometry"]["coordinates"][1]
+    lon = eq["geometry"]["coordinates"][0]
     lats.append(lat)
     lons.append(lon)
     mags.append(mag)
 
-print(lats)
-print(lons)
-print(mags)
+print(lats[:5])
+print(lons[:5])
+print(mags[:5])
+
+from plotly.graph_objs import Scattergeo, Layout
+from plotly import offline
+
+data = [Scattergeo(lon=lons,lat=lats)]
+
+my_layout = Layout(title="Global Earthquakes One Day ")
+
+fig = {'data': data,'layout':my_layout}
+
+offline.plot(fig, filename= 'globalearthquakes1day.html')
